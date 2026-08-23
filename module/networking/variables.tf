e3e08c916f593209a506b00fbf7efc683e02f418
ikigai-crypto <eodekunle52@gmail.com>
2026-08-23 11:49:44 +0100

    Networking variables

--- module/networking/variables.tf
+++ module/networking/variables.tf
@@ -1,15 +1,18 @@
-# Variables required for Networking
+#Tradecore networking variables
 variable "environment" {
   description = "Environment name"
   type        = string
+  default     = "production"
 }
 
 variable "vpc_cidr" {
-  description = "TradeCore VPC CIDR block"
+  description = "Tradecore VPC CIDR block"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
-  description = "Availability zones for the VPC"
+  description = "List of availability zones"
   type        = list(string)
+  default     = ["af-south-1a", "af-south-1b", "af-south-1c"]
 }
