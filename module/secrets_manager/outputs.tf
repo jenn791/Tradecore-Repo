b024bb9b125fbb13a9977efefac0c0052fda8208
ikigai-crypto <eodekunle52@gmail.com>
2026-08-23 13:29:51 +0100

    Add outputs for secret ARNs, IDs, and names

--- module/secrets_manager/outputs.tf
+++ module/secrets_manager/outputs.tf
@@ -1,1 +1,15 @@
+output "secret_arns" {
+  description = "Map of logical secret name to its ARN"
+  value       = { for k, v in aws_secretsmanager_secret.this : k => v.arn }
+}
+
+output "secret_ids" {
+  description = "Map of logical secret name to its ID (name)"
+  value       = { for k, v in aws_secretsmanager_secret.this : k => v.id }
+}
+
+output "secret_names" {
+  description = "Map of logical secret name to its full Secrets Manager name"
+  value       = { for k, v in aws_secretsmanager_secret.this : k => v.name }
+}
 
