969a414354e043abb146abf4fe773bc9b5183f12
ikigai-crypto <eodekunle52@gmail.com>
2026-08-23 13:05:08 +0100

    Define KMS outputs for various encryption keys
    Added outputs for Tradecore and S3, RDS, and backup vault encryption CMKs.

--- module/KMS/outputs.tf
+++ module/KMS/outputs.tf
@@ -1,1 +1,61 @@
+# Tradecore KMS outputs
 
+output "tradecore_key_id" {
+  description = "Key ID of the main Tradecore CMK"
+  value       = aws_kms_key.tradecore.key_id
+}
+
+output "tradecore_key_arn" {
+  description = "ARN of the main Tradecore CMK"
+  value       = aws_kms_key.tradecore.arn
+}
+
+output "tradecore_alias_arn" {
+  description = "ARN of the main Tradecore CMK alias"
+  value       = aws_kms_alias.tradecore.arn
+}
+
+output "s3_key_id" {
+  description = "Key ID of the S3 encryption CMK"
+  value       = aws_kms_key.s3.key_id
+}
+
+output "s3_key_arn" {
+  description = "ARN of the S3 encryption CMK"
+  value       = aws_kms_key.s3.arn
+}
+
+output "s3_alias_arn" {
+  description = "ARN of the S3 encryption CMK alias"
+  value       = aws_kms_alias.s3.arn
+}
+
+output "rds_key_id" {
+  description = "Key ID of the RDS/Aurora encryption CMK"
+  value       = aws_kms_key.rds.key_id
+}
+
+output "rds_key_arn" {
+  description = "ARN of the RDS/Aurora encryption CMK"
+  value       = aws_kms_key.rds.arn
+}
+
+output "rds_alias_arn" {
+  description = "ARN of the RDS/Aurora encryption CMK alias"
+  value       = aws_kms_alias.rds.arn
+}
+
+output "backup_key_id" {
+  description = "Key ID of the backup vault encryption CMK"
+  value       = aws_kms_key.backup.key_id
+}
+
+output "backup_key_arn" {
+  description = "ARN of the backup vault encryption CMK"
+  value       = aws_kms_key.backup.arn
+}
+
+output "backup_alias_arn" {
+  description = "ARN of the backup vault encryption CMK alias"
+  value       = aws_kms_alias.backup.arn
+}
