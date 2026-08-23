302616468a9776a7cd258ff108e0a31ea36f94d9
ikigai-crypto <eodekunle52@gmail.com>
2026-08-23 12:24:44 +0100

    Define S3 module variables for environment and KMS
    Added variables for environment and KMS key ARN.

--- module/s3/variables.tf
+++ module/s3/variables.tf
@@ -1,1 +1,12 @@
+# TradeCore S3 module variables
 
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
+variable "s3_kms_key_arn" {
+  description = "ARN of the KMS key used to encrypt S3 buckets"
+  type        = string
+}
