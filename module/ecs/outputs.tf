05658591edb956387a9e03d0a026432f7b386ca1
CyberNifer <jenniferugwoke151@gmail.com>
2026-08-23 06:59:21 -0400

    Add outputs for ECS cluster and service details

--- module/ecs/outputs.tf
+++ module/ecs/outputs.tf
@@ -1,1 +1,25 @@
+output "cluster_id" {
+  description = "ID of the ECS cluster"
+  value       = aws_ecs_cluster.this.id
+}
+
+output "cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.this.name
+}
+
+output "service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.this.name
+}
+
+output "task_definition_arn" {
+  description = "ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.this.arn
+}
+
+output "security_group_id" {
+  description = "ID of the security group attached to the ECS service"
+  value       = aws_security_group.ecs_service.id
+}
 
