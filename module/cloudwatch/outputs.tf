d32f4cdb20b0e1924924301ea89bf038aa6488ae
CyberNifer <jenniferugwoke151@gmail.com>
2026-08-23 10:06:01 -0400

    Add CloudWatch outputs for log group and alarms

--- module/cloudwatch/outputs.tf
+++ module/cloudwatch/outputs.tf
@@ -1,1 +1,25 @@
+output "log_group_name" {
+  description = "tradecore-cloudwatch"
+  value       = aws_cloudwatch_log_group.tradecore-cloudwatch
+}
+
+output "log_group_arn" {
+  description = "ARN of the CloudWatch log group"
+  value       = aws_cloudwatch_log_group.this.arn
+}
+
+output "sns_topic_arn" {
+  description = "ARN of the SNS topic used for alarm notifications (if created)"
+  value       = var.create_sns_topic ? aws_sns_topic.alerts[0].arn : null
+}
+
+output "alarm_arns" {
+  description = "Map of created CloudWatch alarm names to their ARNs"
+  value       = { for k, v in aws_cloudwatch_metric_alarm.this : k => v.arn }
+}
+
+output "dashboard_arn" {
+  description = "ARN of the CloudWatch dashboard (if created)"
+  value       = var.create_dashboard ? aws_cloudwatch_dashboard.this[0].dashboard_arn : null
+}
 
